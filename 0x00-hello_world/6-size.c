8653f4c93d00aa555b1d16250617524e424b600f
LEKE-AMOS <maineelijah1960@gmail.com>
2022-09-08 16:52:05 -0700

    size 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,28 +1,30 @@
 #!/bin/bash
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  *
- *  * main - entry point
+ *  *main - print size of data
  *
- *   * Return: 0
+ *   *
  *
- *    */
+ *    *Return: Always 0
+ *
+ *     */
 
 int main(void)
 
 {
 
-	 printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
 
-	  printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
 
-	   printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 
-	    printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 
-	     printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-	      return (0);
+	return (0);
 
 }
