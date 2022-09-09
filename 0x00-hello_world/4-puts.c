171a0eb726d31cbe770fab1c44ec3665ebbe0fed
LEKE-AMOS <maineelijah1960@gmail.com>
2022-09-08 17:57:46 -0700

    updated 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
+ * main - main block
  *
- *  * main - main block
+ * Return: 0
  *
- *   * Return: 0
- *
- *    */
-
+ */
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-	 puts("\"Programming is like building a multilingual puzzle");
-
-	  return (0);
-
+	return (0);
 }
