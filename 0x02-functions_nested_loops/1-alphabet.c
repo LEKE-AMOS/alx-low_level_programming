21bac267996de486e9469adfc33f6cd9cebb4905
LEKE-AMOS <maineelijah1960@gmail.com>
2022-09-15 03:52:57 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+/**
+ * *print_alphabet - function that print alphabet
+ *
+ * *it prints in lowercase
+ *
+ * *Return: no return
+ *
+ **/
+
+void print_alphabet(void)
+
+{
+
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	{
+
+		_putchar (letter);
+
+	}
+
+	_putchar (10);
+
+} i
